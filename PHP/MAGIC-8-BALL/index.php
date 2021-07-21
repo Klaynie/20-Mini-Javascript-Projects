5e929056c9bcc48b978319d68dce4b8ccf3b6eb2
Christoph Lenz <coiz@hotmail.de>
2021-07-21 11:58:02 +0200

    Corrected titles

--- PHP/MAGIC-8-BALL/index.php
+++ PHP/MAGIC-8-BALL/index.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
-    <title>PHP Calculator</title>
+    <title>Magic 8-Ball</title>
   </head>
   <body>    
     <div class="container">
